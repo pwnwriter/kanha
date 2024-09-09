541fa0fbc4808585d331b46428855c00ff8646d7
pwnwriter <hey@pwnwriter.xyz>
2024-09-09 09:22:35 -0400

    refactor(nix): just use default nix flake for now

--- flake.nix
+++ flake.nix
@@ -1,28 +1,89 @@
 {
-  description = "Web app pentesting suite written in rust";
+  description = "kanha";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, ... }:
     let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
       forAllSystems =
-        function: nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+
+      darwinDeps =
+        pkgs: with pkgs; [
+          darwin.apple_sdk.frameworks.SystemConfiguration
+          libiconv
+        ];
     in
     {
-      packages = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./default.nix { };
-      });
-
       devShells = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./shell.nix { };
+        default = pkgs.mkShell {
+          name = "bwatch";
+          packages =
+            (with pkgs; [
+              # cargo
+              # cargo-edit
+              # clippy
+              # rustc
+            ])
+            ++ (pkgs.lib.optional pkgs.stdenvNoCC.isDarwin (darwinDeps pkgs));
+        };
+      });
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+      packages = forAllSystems (pkgs: {
+        bwatch =
+          with pkgs;
+          let
+            fs = lib.fileset;
+            sourceFiles = fs.unions [
+              ./Cargo.lock
+              ./Cargo.toml
+              ./src
+            ];
+
+            cargoToml = with builtins; (fromTOML (readFile ./Cargo.toml));
+            pname = cargoToml.package.name;
+            version = cargoToml.package.version;
+            cargoLock.lockFile = ./Cargo.lock;
+            darwinBuildInputs = (darwinDeps pkgs);
+          in
+          pkgs.rustPlatform.buildRustPackage {
+            inherit pname version cargoLock;
+            src = fs.toSource {
+              root = ./.;
+              fileset = sourceFiles;
+            };
+            nativeBuildInputs = [
+              clippy
+              rustfmt
+              openssl
+            ];
+            buildInputs = [ ] ++ lib.optionals stdenv.isDarwin darwinBuildInputs;
+            preBuildPhases = [ "cargoFmt" ];
+            cargoFmt = ''
+              cargo fmt --manifest-path ./Cargo.toml --all --check
+            '';
+            # right after checkPhase (tests)
+            preInstallPhases = [ "clippy" ];
+            clippy = ''
+              cargo clippy -- --deny warnings
+            '';
+          };
+        default = self.packages.${pkgs.system}.bwatch;
+      });
+      apps = forAllSystems (pkgs: {
+        default = {
+          type = "app";
+          program = "${self.packages.${pkgs.system}.bwatch}/bin/bwatch";
+        };
       });
     };
 }
